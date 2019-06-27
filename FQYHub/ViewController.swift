d581318dbd8f28befdefbd2b116bd36024700c76
YinSQ <18620895340@163.com>
2019-06-27 10:17:22 +0800

    Initial Commit

---
+++ FQYHub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FQYHub
+//
+//  Created by yinsiqian on 2019/6/27.
+//  Copyright © 2019 FengQingYang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
